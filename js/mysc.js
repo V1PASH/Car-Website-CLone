e7060a6627b2f313d4952aca2788843655e426f4
Arshpreet Singh <arshpreets2000@gmail.com>
2025-01-30 23:56:03 +0530

    updates

--- js/mysc.js
+++ js/mysc.js
@@ -44,3 +44,107 @@ function bg3() {
 
     }
 }
+
+
+
+window.onload = function() {
+
+    function changeImage() {
+        i = (i + 1) % 3
+        document.getElementById('first').style.backgroundImage = back[i];
+        if (i == 0) {
+            document.getElementById('bg-1').style.backgroundColor = 'orange';
+            document.getElementById('bg-2').style.backgroundColor = 'transparent';
+            document.getElementById('bg-3').style.backgroundColor = 'transparent';
+            document.getElementById('main-tag-price').innerText = '$1000';
+            document.getElementById('car-name').innerText = 'Bugatti Mistral';
+        }
+        if (i == 1) {
+            document.getElementById('bg-1').style.backgroundColor = 'transparent';
+            document.getElementById('bg-2').style.backgroundColor = 'orange';
+            document.getElementById('bg-3').style.backgroundColor = 'transparent';
+            document.getElementById('main-tag-price').innerText = '$450';
+            document.getElementById('car-name').innerText = 'Audi RS7 SportBack';
+        }
+        if (i == 2) {
+            document.getElementById('bg-1').style.backgroundColor = 'transparent';
+            document.getElementById('bg-2').style.backgroundColor = 'transparent';
+            document.getElementById('bg-3').style.backgroundColor = 'orange';
+            document.getElementById('main-tag-price').innerText = '$650';
+            document.getElementById('car-name').innerText = 'Bentley Bentayga';
+
+        }
+    }
+    window.setInterval(changeImage, 5000);
+}
+
+/*services*/
+function services1() {
+    document.getElementById('not-active0').style.visibility = 'hidden';
+    document.getElementById('not-active0').style.display = 'none';
+    document.getElementById('not-active0').style.maxWidth = '0';
+    document.getElementById('not-active0').style.opacity = '0';
+
+    document.getElementById('not-active1').style.visibility = 'hidden';
+    document.getElementById('not-active1').style.display = 'none';
+    document.getElementById('not-active1').style.maxWidth = '0';
+    document.getElementById('not-active1').style.opacity = '0';
+
+    document.getElementById('not-active2').style.visibility = 'hidden';
+    document.getElementById('not-active2').style.display = 'none';
+    document.getElementById('not-active2').style.maxWidth = '0';
+    document.getElementById('not-active2').style.opacity = '0';
+
+
+
+    document.getElementById('active0').style.display = 'block';
+    document.getElementById('active0').style.visibility = 'visible';
+    document.getElementById('active0').style.maxWidth = 'fit-content';
+    document.getElementById('active0').style.opacity = '1';
+
+    document.getElementById('active1').style.visibility = 'visible';
+    document.getElementById('active1').style.display = 'block';
+    document.getElementById('active1').style.maxWidth = 'fit-content';
+    document.getElementById('active1').style.opacity = '1';
+
+    document.getElementById('active2').style.visibility = 'visible';
+    document.getElementById('active2').style.display = 'block';
+    document.getElementById('active2').style.maxWidth = 'fit-content';
+    document.getElementById('active2').style.opacity = '1';
+
+}
+
+function services2() {
+    document.getElementById('not-active0').style.visibility = 'visible';
+    document.getElementById('not-active0').style.display = 'block';
+    document.getElementById('not-active0').style.maxWidth = 'fit-content';
+    document.getElementById('not-active0').style.opacity = '1';
+
+    document.getElementById('not-active1').style.visibility = 'visible';
+    document.getElementById('not-active1').style.display = 'block';
+    document.getElementById('not-active1').style.maxWidth = 'fit-content';
+    document.getElementById('not-active1').style.opacity = '1';
+
+    document.getElementById('not-active2').style.visibility = 'visible';
+    document.getElementById('not-active2').style.display = 'block';
+    document.getElementById('not-active2').style.maxWidth = 'fit-content';
+    document.getElementById('not-active2').style.opacity = '1';
+
+
+
+    document.getElementById('active0').style.display = 'none';
+    document.getElementById('active0').style.visibility = 'hidden';
+    document.getElementById('active0').style.maxWidth = '0';
+    document.getElementById('active0').style.opacity = '0';
+
+    document.getElementById('active1').style.visibility = 'hidden';
+    document.getElementById('active1').style.display = 'none';
+    document.getElementById('active1').style.maxWidth = '0';
+    document.getElementById('active1').style.opacity = '0';
+
+    document.getElementById('active2').style.visibility = 'hidden';
+    document.getElementById('active2').style.display = 'none';
+    document.getElementById('active2').style.maxWidth = '0';
+    document.getElementById('active2').style.opacity = '0';
+
+}
